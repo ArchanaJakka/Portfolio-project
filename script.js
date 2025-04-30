f36837a4c77285540effa1b511209e373b0fc58d
ArchanaJakka <archanajakka22@gmail.com>
2025-04-30 10:08:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll
+document.querySelectorAll("nav a").forEach(link => {
+    link.addEventListener("click", e => {
+      e.preventDefault();
+      const section = document.querySelector(link.getAttribute("href"));
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+  
+  
